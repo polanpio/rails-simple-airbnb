a9fb59de8b2b0dd35f770cbf49631b8c7f90c989
Piotr Polanski <polanski.pio@gmail.com>
2021-07-08 20:21:41 +0900

    styles for index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+Flat.destroy_all
+
 puts 'Start creating flats'
 puts 'Flat 1'
 Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  picture_url: 'https://source.unsplash.com/1600x900/?london,apartment',
   price_per_night: 75,
   number_of_guests: 3
 )
@@ -22,6 +25,7 @@ Flat.create!(
   name: 'Bijoux yet luxurious -Belgravia',
   address: '31 St Barnabas St London SW1W 8QB',
   description: 'Charming town house with two double bedrooms, situated in the elegant London ward of Belgravia.',
+  picture_url: 'https://source.unsplash.com/1600x900/?london,apartment',
   price_per_night: 61,
   number_of_guests: 2
 )
@@ -31,6 +35,7 @@ Flat.create!(
   name: 'Stylish Entire 1 Bedroom Flat',
   address: '3 Tetcott Rd London SW10 0SA',
   description: "Beautifully furnished flat on the King's Road, perfect for couple getaways to London or for the overnight business stopover. High-end furnishing, quaint balcony perfect for an evening drink in the summer, along with a park steps from your doorstep.",
+  picture_url: 'https://source.unsplash.com/1600x900/?london,apartment',
   price_per_night: 76,
   number_of_guests: 2
 )
@@ -40,6 +45,7 @@ Flat.create!(
   name: 'Cheval Place for 3 people.',
   address: '55 Brompton Square London SW3 2AG',
   description: '2-room terraced house on 2 levels. Comfortable and cosy furnishings: 1 room with 1 french bed and radio.',
+  picture_url: 'https://source.unsplash.com/1600x900/?london,apartment',
   price_per_night: 9,
   number_of_guests: 3
 )
