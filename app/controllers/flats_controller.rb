56b0535e0cbc36304558172acc62da68edfe353f
Piotr Polanski <polanski.pio@gmail.com>
2021-07-08 19:31:45 +0900

    created basic structure

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,37 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Start creating flats'
+puts 'Flat 1'
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+puts 'Flat 2'
+Flat.create!(
+  name: 'Bijoux yet luxurious -Belgravia',
+  address: '31 St Barnabas St London SW1W 8QB',
+  description: 'Charming town house with two double bedrooms, situated in the elegant London ward of Belgravia.',
+  price_per_night: 61,
+  number_of_guests: 2
+)
+
+puts 'Flat 3'
+Flat.create!(
+  name: 'Stylish Entire 1 Bedroom Flat',
+  address: '3 Tetcott Rd London SW10 0SA',
+  description: "Beautifully furnished flat on the King's Road, perfect for couple getaways to London or for the overnight business stopover. High-end furnishing, quaint balcony perfect for an evening drink in the summer, along with a park steps from your doorstep.",
+  price_per_night: 76,
+  number_of_guests: 2
+)
+
+puts 'Flat 4'
+Flat.create!(
+  name: 'Cheval Place for 3 people.',
+  address: '55 Brompton Square London SW3 2AG',
+  description: '2-room terraced house on 2 levels. Comfortable and cosy furnishings: 1 room with 1 french bed and radio.',
+  price_per_night: 9,
+  number_of_guests: 3
+)
+puts 'Flats created'
